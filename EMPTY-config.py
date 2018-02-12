9d6fb4086fa94e4837f296f96059d61b2435094f
Kris Harrison <krissirk@Kriss-MacBook-Air.local>
2018-02-12 15:32:02 -0800

    Comments added to the empty-config file to clarify its purpose.

--- EMPTY-config.py
+++ EMPTY-config.py
@@ -1,3 +1,9 @@
+"""
+This file is a sample *template* of the 'config.py' file that holds variables
+storing IDs enabling access to the Trello API and relevant entities within my
+job search board. The IDs live as local environment variables on Heroku and are
+setup manually.
+"""
 TRELLO_API_KEY = ""
 TRELLO_API_TOKEN = ""
 TRELLO_APPLIED_LIST_ID = ""
