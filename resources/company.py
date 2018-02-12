915485392861916f5275b4771cd55eab475697a7
Kris Harrison <krissirk@Kriss-MacBook-Air.local>
2018-02-12 11:42:23 -0800

    Reverted debug code from trello_api_app file and reorganized retreival and setting of secret IDs

--- resources/company.py
+++ resources/company.py
@@ -14,23 +14,23 @@ from collections import Counter
 
 import os, trello_request
 
-# URL for Trello batch API comprised of IDs stored in config.py file (or server
-# envrionment variables) - probably a better way to do this
+# Initialize URL for Trello batch API comprised of IDs stored in config.py file
+# (or server envrionment variables) - probably a better way to do this
 try:
     from config import *
-    trelloApiUrl = "https://api.trello.com/1/batch?urls=/lists/{0}/cards,/lists/{1}/cards,/lists/{2}/cards&key={3}&token={4}".format(
-                    TRELLO_APPLIED_LIST_ID,
-                    TRELLO_INTERVIEWING_LIST_ID,
-                    TRELLO_CLOSED_LIST_ID,
-                    TRELLO_API_KEY,
-                    TRELLO_API_TOKEN)
 except:
-    trelloApiUrl = "https://api.trello.com/1/batch?urls=/lists/{0}/cards,/lists/{1}/cards,/lists/{2}/cards&key={3}&token={4}".format(
-                    os.environ.get('TRELLO_APPLIED_LIST_ID'),
-                    os.environ.get('TRELLO_INTERVIEWING_LIST_ID'),
-                    os.environ.get('TRELLO_CLOSED_LIST_ID'),
-                    os.environ.get('TRELLO_API_KEY'),
-                    os.environ.get('TRELLO_API_TOKEN'))
+    TRELLO_APPLIED_LIST_ID = os.environ.get('TRELLO_APPLIED_LIST_ID')
+    TRELLO_INTERVIEWING_LIST_ID = os.environ.get('TRELLO_INTERVIEWING_LIST_ID')
+    TRELLO_CLOSED_LIST_ID = os.environ.get('TRELLO_CLOSED_LIST_ID')
+    TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
+    TRELLO_API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
+
+trelloApiUrl = "https://api.trello.com/1/batch?urls=/lists/{0}/cards,/lists/{1}/cards,/lists/{2}/cards&key={3}&token={4}".format(
+                TRELLO_APPLIED_LIST_ID,
+                TRELLO_INTERVIEWING_LIST_ID,
+                TRELLO_CLOSED_LIST_ID,
+                TRELLO_API_KEY,
+                TRELLO_API_TOKEN)
 
 # Companies resource featuring a single Get endpoint
 class Companies(Resource):
