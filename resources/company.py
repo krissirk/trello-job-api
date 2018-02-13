bfb5beee3a816d321cfb5341aba9bef60191737d
Kris Harrison <krissirk@Kriss-MacBook-Air.local>
2018-02-12 18:52:39 -0800

    re-organized the if/else logic

--- resources/company.py
+++ resources/company.py
@@ -40,37 +40,39 @@ class Companies(Resource):
         # Trello API request
         trelloResponse, trelloStatusCode = trello_request.getApiResponse(trelloApiUrl)
 
-        # Return an error if the Trello API request fails
-        if trelloStatusCode != 200:
+        # Build JSON of company data and return it; else, return an error
+        # if the Trello API request fails
+        if trelloStatusCode == 200:
+
+            #Initialize a list that will store each company value in the Trello JSON
+            companies = []
+
+            for lists in trelloResponse.json():
+
+                # The batch JSON response includes three 'list' groupings of Trello
+                # cards; one for each workflow step of job search exploration
+                for cards in lists['200']:
+
+                    # Append the name of each Trello card to the list; the Trello
+                    # card naming convention is <Company> - <Position Name>, so the
+                    # card value after the ' -' is stripped away
+                    companies.append(cards['name'].split('-')[0].strip())
+
+            # Leverage the Counter object to get the count of each instance of a given
+            # company name in the list; cast it to be a proper dictionary object
+            countedCompanies = dict(Counter(companies))
+
+            # Sort the companies dictionary by name
+            sortedCompanies = sorted(countedCompanies.items(), key=operator.itemgetter(0))
+
+            # Initialize the JSON for the Get endpoint and include a total count of
+            # the companies in the response
+            companyJSON = {'companies': [], 'totalCompanies': len(sortedCompanies)}
+
+            # Put each company and its count into the JSON
+            for company in sortedCompanies:
+                companyJSON['companies'].append({'name': company[0], 'totalCount': company[1]})
+
+            return companyJSON
+        else:
             return {"message": "An error occurred retrieving the Companies data from Trello."}, 500
-
-        #Initialize a list that will store each company value in the Trello JSON
-        companies = []
-
-        for lists in trelloResponse.json():
-
-            # The batch JSON response includes three 'list' groupings of Trello
-            # cards; one for each workflow step of job search exploration
-            for cards in lists['200']:
-
-                # Append the name of each Trello card to the list; the Trello
-                # card naming convention is <Company> - <Position Name>, so the
-                # card value after the ' -' is stripped away
-                companies.append(cards['name'].split('-')[0].strip())
-
-        # Leverage the Counter object to get the count of each instance of a given
-        # company name in the list; cast it to be a proper dictionary object
-        countedCompanies = dict(Counter(companies))
-
-        # Sort the companies dictionary by name
-        sortedCompanies = sorted(countedCompanies.items(), key=operator.itemgetter(0))
-
-        # Initialize the JSON for the Get endpoint and include a total count of
-        # the companies in the response
-        companyJSON = {'companies': [], 'totalCompanies': len(sortedCompanies)}
-
-        # Put each company and its count into the JSON
-        for company in sortedCompanies:
-            companyJSON['companies'].append({'name': company[0], 'totalCount': company[1]})
-
-        return companyJSON
